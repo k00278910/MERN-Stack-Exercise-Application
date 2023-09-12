7669de736a2f58601f84ccee75cbf3570ed2d4e4
k00278910 <k00278910@student.lit.ie>
2023-09-12 14:51:14 +0100

    set up a database using MongoDB Atlas, and then connect to application using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 // server.js is the back-end entry point
 //  must register the express application inside the server js file 
 const express = require ('express')
+// mongoose wraps the code for mongoDB compilation (methods to read/write DB documents)
+// can create rules in order for data to be accepted/rejected by the schema
+const mongoose = require ('mongoose')
 const workoutRouts = require ('./routs/workouts')
 // create express app
 const app = express()
@@ -18,12 +21,20 @@ next() // allows program to progress
 
 // * ROUTES (attaches all the routs to the app)
 app.use('/api/workouts', workoutRouts)
-
-// * LISTEN
+// * CONNECT to DB
+mongoose.connect(process.env.MONGO_URI)
+// Function to show completed connection
+.then(()=>{
+    // * LISTEN
 // listen on port, then fire a function */
 app.listen(process.env.PORT, ()=>{
-    console.log('listening on port' , process.env.PORT)
+    console.log('connected to DB & listening on port' , process.env.PORT)
 })
+})
+// Check for connection error
+.catch((error)=>{console.log(error)})
+
+
 
 
 
