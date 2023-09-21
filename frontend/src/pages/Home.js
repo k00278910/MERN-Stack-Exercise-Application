e827016e46039ed374428fb368129e72e830fbe5
k00278910 <k00278910@student.lit.ie>
2023-09-21 21:00:19 +0100

    fetch data from workout database

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,36 @@
+import {useEffect, useState} from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () => {
+// 2- the workout after 1
+const [workouts,setWorkouts] = useState(null)
+
+// 1- fire this function when the component first renders
+// we then have the 'workouts' at 2  
+useEffect (()=>{
+    const fetchWorkouts = async ()=>{
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+      if(response.ok){
+        setWorkouts(json)
+      }
+
+    }
+    fetchWorkouts()
+
+  },[])
 
     return (
       <div className="home">
-        <h2>Home</h2>
+     
+      <div className="workouts"></div>
+        {workouts&&workouts.map((workout)=>(
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        )
+          
+          )}
       </div>
     )
   }
   
-  export default Home
+  export default Home;
